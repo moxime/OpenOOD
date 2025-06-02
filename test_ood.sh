42f50a579d88c8e84b4c3407dbb3ddd8641db843
moxime <mossonce@hotmail.com>
2025-06-02 11:48:37 +0200

    test ood scripts

---
+++ test_ood.sh
@@ -0,0 +1,9 @@
+#! env bash
+
+if [ -z $1 ]
+then
+    cd configs/postprocessors/
+    ls *.yml
+else
+    python main.py --config configs/datasets/cifar10/cifar10.yml configs/datasets/cifar10/cifar10_ood.yml configs/preprocessors/base_preprocessor.yml configs/networks/vgg19_32x32.yml configs/postprocessors/$1.yml configs/pipelines/test/test_ood.yml --seed 0
+fi
