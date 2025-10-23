ee43b209dfd2da3c3ab799414233c47e2af9fe41
moxime <mossonce@hotmail.com>
2025-10-23 10:37:52 +0200

    tta postprocessor

---
+++ openood/postprocessors/tta_postprocessor.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.metrics import pairwise_distances_argmin_min
+import scipy
+from tqdm import tqdm
+
+from .base_postprocessor import BasePostprocessor
+from .info import num_classes_dict
+
+
+class TTAPostprocessor(BasePostprocessor):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_classes = num_classes_dict[self.config.dataset.name]
+        self.args = self.config.postprocessor.postprocessor_args
+        self.args_dict = self.config.postprocessor.postprocessor_sweep
+        # self.alpha = self.args.alpha
+        self.setup_flag = False
+
+    def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
+        if self.setup_flag:
+            return
+        for cat in dir(self.config):
+            print(cat)
+
+    def process_batch(self, net, data):
+        pass
+
+    @torch.no_grad()
+    def postprocess(self, net: nn.Module, data: Any):
+        logits = net(data)
+        preds = logits.argmax(1)
+        softmax = F.softmax(logits, 1).cpu().numpy()
+        scores = -pairwise_distances_argmin_min(
+            softmax, np.array(self.mean_softmax_val), metric=self.kl)[1]
+        return preds, torch.from_numpy(scores)
