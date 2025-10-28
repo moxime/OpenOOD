fa5baf5ed255498cfcacc6aa01575c339d28b0f0
moxime <mossonce@hotmail.com>
2025-10-28 18:58:03 +0100

    slurm job array to be tested

--- scripts/slurm-job.sh
+++ scripts/slurm-job.sh
@@ -7,6 +7,38 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=2-00:00:00
 #SBATCH --constraint=v100
+#SBATCH --array=0-10
+
+methods=("msp" "odin" "mds"  "ebo" "gradnorm" "vim" "react" "ash") 
+
+dataset=imagenet
+
+while :; do
+    case $1 in
+	-m )
+	    methods=()
+	    while [[ "$2" != -* ]]; do
+		if [ -z "$2" ]
+		then
+		    break
+		fi
+		# echo processing $2 for mean
+		methods+=($2)
+		shift
+	    done
+	    ;;
+	-d )
+	    shift
+	    dataset=$1
+	    ;;
+	* )
+	    break
+	    ;;
+    esac
+    shift
+done
+
+
 
 echo $SLURM_JOB_ID is started
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -19,8 +51,8 @@ export CUDA_LAUNCH_BLOCKING=1
 
 echo "started job by $(whoami)"
 
-nvidia-smi
-nvidia-smi -L
+# nvidia-smi
+# nvidia-smi -L
 
 ulimit -n 4096
 echo "setting ulimit -n to $(ulimit -n)"
@@ -31,29 +63,25 @@ seed=0
 unset ckpt
 
 # id / network: $1
-case $1 in
+case $dataset in
 
     cifar10)
-	idset=cifar10
 	network=resnet18_32x32
 	ckpt_suffix=base_e100_lr0.1_default
     ;;
 
     cifar100)
-	idset=cifar100
 	network=resnet18_32x32
 	ckpt_suffix=base_e100_lr0.1_default
     ;;
 
     imagenet)
-	idset=imagenet
 	network=resnet50
 	ckpt="checkpoints/resnet50-0676ba61.pth"
 
     ;;
 
     imagenet200)
-	idset=imagenet200
 	network=resnet18_224x224
 	ckpt_suffix=base_e90_lr0.1_default
     ;;
@@ -61,28 +89,31 @@ case $1 in
 esac
 
 if [ -z $ckpt ] ; then
-    ckpt=checkpoints/"$idset"_"$network"_"$ckpt_suffix"/s"$seed"/best.ckpt
+    ckpt=checkpoints/"$dataset"_"$network"_"$ckpt_suffix"/s"$seed"/best.ckpt
 fi
 
-method=$2
 
-echo ckpt: $ckpt
+num_of_methods=${#methods[@]}
 
-ls configs/datasets/$idset/$idset.yml \
+if [ $SLURM_ARRAY_TASK_ID -lt $num_of_methods ] ; then
+
+    method=${methods[$SLURM_ARRAY_TASK_ID]}
+
+    ls -1 configs/datasets/$dataset/$dataset.yml \
        configs/networks/$network.yml \
-       configs/datasets/$idset/"$idset"_ood.yml \
+       configs/datasets/$dataset/"$dataset"_ood.yml \
        configs/preprocessors/base_preprocessor.yml \
        configs/postprocessors/$method.yml \
        configs/pipelines/test/test_ood.yml \
        $ckpt
 
-echo python main.py --config configs/datasets/$idset/$idset.yml \
-       configs/networks/$network.yml \
-       configs/datasets/$idset/"$idset"_ood.yml \
-       configs/preprocessors/base_preprocessor.yml \
-       configs/postprocessors/$method.yml \
-       configs/pipelines/test/test_ood.yml \
-       --seed $seed \
-       --mark $SLURM_JOB_ID
+    echo python main.py --config configs/datasets/$dataset/$dataset.yml \
+	 configs/networks/$network.yml \
+	 configs/datasets/$dataset/"$dataset"_ood.yml \
+	 configs/preprocessors/base_preprocessor.yml \
+	 configs/postprocessors/$method.yml \
+	 configs/pipelines/test/test_ood.yml \
+	 --seed $seed \
+	 --mark $SLURM_JOB_ID
 
-
+fi
