b8ec1a53778afd14121955932012e15c4d51df7e
moxime <mossonce@hotmail.com>
2025-11-26 15:55:08 +0100

    reset and finetune in tta_ood_postprocessor

--- openood/postprocessors/tta_postprocessor.py
+++ openood/postprocessors/tta_postprocessor.py
@@ -42,59 +42,86 @@ class TTAPostprocessor(BasePostprocessor):
     def __init__(self, config):
         super().__init__(config)
 
+        # batch_size for fine_tuning
+        self.batch_size = self.config.ood_dataset.batch_size
+        try:
+            self.batch_size = self.config.postprocessor.batch_size
+        except AttributeError:
+            pass
+
+        try:
+            self.reset_net_at_chunk = self.config.postprocessor.reset_network
+        except AttributeError:
+            self.reset_net_at_chunk = True
+
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
-        if self.setup_flag:
-            return
+        """setup is done once (for instance, get some metrics on the
+        training ind dataset
 
-        self.setup_flag = True
+        """
+        if self.reset_net_at_chunk:
+            self.reset_net_at_chunk = os.path.join(self.config.output_dir, 'tmp.ckpt')
+            net.save(self.reset_net_at_chunk)
 
-    def postprocess(self, net: nn.Module, data: Any):
+    def reset(net, data_loader):
+        """reset is done at each new "experiment" (dataset)
 
-        with torch.no_grad():
-            output = net(data)
-            score = torch.softmax(output, dim=1)
-            conf, pred = torch.max(score, dim=1)
+        for instance, if psotprocessor manages an history dict, this
+        might be the place to do it (by overriding this method)
 
-        """if there is some finentuning, do it here (will be done tta_epochs +1)
-            
-        - the first result is for 0 finetuning
-            
-        - the last result yielded will be after tta_epochs finetuning
-            
+        """
+        pass
+
+    @torch.no_grad()
+    def postprocess(self, net: nn.Module, data: Any, epoch=0):
+        """ postprocess is done for each "chunk" of data at each epoch
         """
 
-        minibatch_loader = DataLoader(data, shuffle=True,
-                                      batch_size=self.config.postprocessor.tta_batch_size)
-        self.finetune(net, minibatch_loader)
+        if epoch == 0 and self.reset_net_at_chunk:
+            net.load(self.reset_net_at_chunk)
+
+        output = net(data)
+        score = torch.softmax(output, dim=1)
+        conf, pred = torch.max(score, dim=1)
+
+        if epoch == 0:
+            self.predicted_labels = pred
 
         return pred, conf
 
-    def finetune(self, net, data):
+    def finetune(self, net, data, epoch=0):
+        """finetune is done after postprocess (that way you can
+        retrieve results before any finetuning ; that's why
+        postprocess is done epochs+1 times, to benefit from n=epochs
+        finetuning)
+
+        - minibatch_loader is a shuffled datalpder on the processed
+        chunk with batch_size=self.batch_size
+
+        """
+
+        # for instance you can create a minibatch_loader
+        minibatch_loader = DataLoader(data, shuffle=True, batch_size=self.batch_size)
+        # ...
         pass
 
     def inference(self,
                   net: nn.Module,
                   data_loader: DataLoader,
+                  epoch=0,
                   epochs=0,
                   progress: bool = True):
 
         lists = {_: {} for _ in ('pred', 'conf', 'label')}
 
-        try:
-            reset_net_at_epoch = self.config.postprocessor.reset_network
-        except AttributeError:
-            reset_net_at_epoch = True
-        if reset_net_at_epoch:
-            reset_net_at_epoch = os.path.join(self.config.output_dir, 'tmp.ckpt')
-            net.save(reset_net_at_epoch)
+        self.reset(net, data_loader)
         for batch in tqdm(data_loader,
                           disable=not progress or not comm.is_main_process()):
-            if reset_net_at_epoch:
-                net.load(reset_net_at_epoch)
             for epoch in range(epochs+1):
                 data = batch['data'].cuda()
                 label = batch['label'].cuda()
-                pred, conf = self.postprocess(net, data)
+                pred, conf = self.postprocess(net, data, epoch=epoch)
+                self.finetune(net, data, batch_size=self.batch_size, epoch=epoch)
 
                 for key, tensor in zip(('pred', 'conf', 'label'), (pred, conf, label)):
                     lists[key].setdefault(epoch, []).append(tensor.cpu())
