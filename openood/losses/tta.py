3b656c676b9972c31fa2179c4be4b41f1aa4f852
moxime <mossonce@hotmail.com>
2026-03-09 14:46:59 +0100

    with margin in ce loss for resnet20

--- openood/losses/tta.py
+++ openood/losses/tta.py
@@ -1,3 +1,4 @@
+import torch.nn as nn
 import torch
 
 
@@ -20,3 +21,46 @@ def uniform_ce(logits, **kw):
 
         """
     return -logits.mean(-1) + logits.logsumexp(-1)
+
+
+class MarginCrossEntropy(nn.CrossEntropyLoss):
+
+    def __init__(self, *a, margin=0, **kw):
+
+        super().__init__(*a, **kw)
+        self.margin = margin
+
+    def forward(self, input, target):
+
+        if self.margin:
+            margin = 1. + torch.zeros_like(input).scatter_(1, target.unsqueeze(1), self.margin)
+        else:
+            margin = 1.
+
+        return super().forward(input * margin, target)
+
+
+if __name__ == '__main__':
+
+    import argparse
+
+    N = 100
+    C = 10
+    K = 16
+
+    logits = torch.randn(N, C)
+    labels = torch.randint(C, (N,))
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('margin', type=float)
+    parser.add_argument('-a', type=float, default=20)
+
+    args = parser.parse_args()
+    logits += torch.zeros_like(logits).scatter_(1, labels.unsqueeze(1), args.a)
+
+    loss = MarginCrossEntropy(margin=args.margin)
+
+    msp = logits.softmax(-1).max(-1)[0]
+
+    print(' -- '.join(map('{:.1%}'.format, msp[:10])))
+    print('L = {:.1g}'.format(loss(logits, labels)))
