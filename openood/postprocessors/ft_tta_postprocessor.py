71eed7e08100720e43edfcca09eb981c240a2514
moxime <mossonce@hotmail.com>
2026-04-16 13:00:08 +0200

    debug dset name

--- openood/postprocessors/ft_tta_postprocessor.py
+++ openood/postprocessors/ft_tta_postprocessor.py
@@ -65,15 +65,19 @@ class FTTTAPostprocessor(TTAPostprocessor):
                 if name.lower().startswith('layer') and unfreeze == 'penultimate':
                     break
 
+        for _ in self.aux_dls:
+
+            print('*** axu_dl[]: {}'.format(_, self.aux_dls[_].dataset)
+
     def next_aux_minibatch(self, where):
 
         assert where in self.stratified, '{} is ot stratified'.format(where)
         try:
             return next(self._aux_iters[where])
         except AttributeError:
-            self._aux_iters = {}
+            self._aux_iters={}
         except (KeyError, StopIteration):
-            self._aux_iters[where] = iter(self.aux_dls[where])
+            self._aux_iters[where]=iter(self.aux_dls[where])
 
         return self.next_aux_minibatch(where)
 
@@ -101,11 +105,11 @@ class FTTTAPostprocessor(TTAPostprocessor):
             return
 
         if not hasattr(self, '_inspector'):
-            self._inspector = BatchInspector()
+            self._inspector=BatchInspector()
 
-        self._inspector.epoch = epoch
+        self._inspector.epoch=epoch
 
-        printout = (not epoch or self.calculate_conf(epoch+1, epochs)) and flush
+        printout=(not epoch or self.calculate_conf(epoch+1, epochs)) and flush
         self._inspector.update_mb(epoch, epochs=epochs, printout=printout, **kw)
         if printout:
             self._inspector.print()
@@ -115,95 +119,95 @@ class FTTTAPostprocessor(TTAPostprocessor):
 
         """
 
-        mix_batch = {'conf': conf, 'pred': pred, 'data': data, 'where': ['mix' for _ in pred]}
-        batch_list = [dict(zip(mix_batch, t)) for t in zip(*mix_batch.values())]
+        mix_batch={'conf': conf, 'pred': pred, 'data': data, 'where': ['mix' for _ in pred]}
+        batch_list=[dict(zip(mix_batch, t)) for t in zip(*mix_batch.values())]
 
         for where in self.pad_buffers:
             batch_list.extend(self.pad_buffers[where])
 
         for d in batch_list:
             d.pop('weights', None)
-            d['weights'] = torch.tensor(self.losses_weight(**d, epoch=epoch, epochs=epochs))
+            d['weights']=torch.tensor(self.losses_weight(**d, epoch=epoch, epochs=epochs))
 
         if self.filterout_null_weights:
-            batch_list = [_ for _ in batch_list if _['weights'].norm()]
+            batch_list=[_ for _ in batch_list if _['weights'].norm()]
 
         if self.size_normalization:
-            N_samples = len(batch_list)
-            N_id_weights = len([_ for _ in batch_list if _['weights'][0] > 0])
-            N_adaptation_weights = len([_ for _ in batch_list if _['weights'][1] > 0])
+            N_samples=len(batch_list)
+            N_id_weights=len([_ for _ in batch_list if _['weights'][0] > 0])
+            N_adaptation_weights=len([_ for _ in batch_list if _['weights'][1] > 0])
 
-            w_normalization = (N_samples / (N_id_weights + 1e-12),
+            w_normalization=(N_samples / (N_id_weights + 1e-12),
                                N_samples / (N_adaptation_weights + 1e-12))
 
         else:
-            w_normalization = (1., 1.)
+            w_normalization=(1., 1.)
 
-        minibatch_loader = DataLoader(batch_list,
+        minibatch_loader=DataLoader(batch_list,
                                       shuffle=batch_list,  # if batch_list is empty, do not shuffle (bug)
                                       batch_size=self.batch_size,
                                       drop_last=False)
 
         if epoch == 0:
             # to track clipped_grad (removed, see below)
-            self._clipped_grad = 0
-            self._grad = 0
+            self._clipped_grad=0
+            self._grad=0
 
         for i, batch in enumerate(minibatch_loader):
 
-            inspection_dict = {}
+            inspection_dict={}
 
-            data = batch['data'].cuda()
-            pred = batch['pred'].cuda()
-            conf = batch['conf'].cuda()
-            where = batch['where']
+            data=batch['data'].cuda()
+            pred=batch['pred'].cuda()
+            conf=batch['conf'].cuda()
+            where=batch['where']
 
-            weights = batch['weights'].cuda()
-            w_norm0 = weights.norm(0, dim=0)
+            weights=batch['weights'].cuda()
+            w_norm0=weights.norm(0, dim=0)
 
             inspection_dict.update(where=where, conf=conf)
 
             if any(conf.isnan()):
                 raise ValueError('{} NaN in conf'.format(conf.isnan().int().sum()))
 
-            logits, features = net(data, return_feature=True)
+            logits, features=net(data, return_feature=True)
 
             if w_norm0[0]:
-                id_loss = self.loss(logits, pred)
+                id_loss=self.loss(logits, pred)
             else:
-                id_loss = torch.zeros_like(conf)
+                id_loss=torch.zeros_like(conf)
 
             if w_norm0[1]:
-                adaptation_loss = self.adaptation_loss(logits=logits, features=features, net=net)
+                adaptation_loss=self.adaptation_loss(logits=logits, features=features, net=net)
             else:
-                adaptation_loss = torch.zeros_like(conf)
+                adaptation_loss=torch.zeros_like(conf)
 
             inspection_dict.update(id_loss=id_loss, adaptation_loss=adaptation_loss, weights=weights)
 
             self.optimizer.zero_grad()
 
             # weights is batch_size*2
-            loss = w_normalization[0] * (id_loss * weights.T[0]).mean()
+            loss=w_normalization[0] * (id_loss * weights.T[0]).mean()
             loss += w_normalization[1] * (adaptation_loss * weights.T[1]).mean()
 
             for _ in self.stratified:
-                batch_ = self.next_aux_minibatch(_)
-                data = batch_['data'].cuda()
-                logits, features = net(data, return_feature=True)
+                batch_=self.next_aux_minibatch(_)
+                data=batch_['data'].cuda()
+                logits, features=net(data, return_feature=True)
                 if _ == 'ood':
-                    w = self.beta
-                    stratified_loss = self.adaptation_loss(logits, features, net)
+                    w=self.beta
+                    stratified_loss=self.adaptation_loss(logits, features, net)
                 elif _ == 'id':
-                    w = 1
-                    pred = batch_['label'].cuda()
-                    stratified_loss = self.loss(logits, pred)
+                    w=1
+                    pred=batch_['label'].cuda()
+                    stratified_loss=self.loss(logits, pred)
 
                 inspection_dict.update({'stratified_loss_{}'.format(_): stratified_loss})
                 loss += (w * stratified_loss).mean()
 
             loss.backward()
 
-            grad_norm = torch.nn.utils.clip_grad_norm_(net.parameters(), 200)
+            grad_norm=torch.nn.utils.clip_grad_norm_(net.parameters(), 200)
             if grad_norm > 200:
                 self._clipped_grad += 1
             self._grad += 1
