2d2e6a0b50f6a7b7c139ff69ef0ad18bbdadc8aa
moxime <mossonce@hotmail.com>
2026-06-16 11:52:51 +0200

    bugfix

--- openood/postprocessors/ft_dist_tta_postprocessor.py
+++ openood/postprocessors/ft_dist_tta_postprocessor.py
@@ -50,17 +50,19 @@ class DistTTAPostprocessor(FTTTAPostprocessor):
         debug = self.debug
         # self.debug = 0
         # output : pred[conf], conf[epoch], label[epoch]
+        t = self.pad_thresholds['self']
+        self.pad_thresholds['self'] = -np.inf
         outputs = self.inference(net, id_loader_dict['val'], epochs=self.epochs)
         for epoch in outputs[0]:
             pred, conf, label = (_[epoch] for _ in outputs)
             q = [0.1, 0.5, 0.9]
             quantiles = {_: np.quantile(conf, _) for _ in q}
-            t = self.thresholds['self']
             self_prop = (conf < t).mean()
             print('*** val q {}/{}'.format(epoch, self.epochs),
                   ' '.join('{}:{:.3f}'.format(*i) for i in quantiles.items()),
                   '{:.1%} < {}'.format(self_prop, t))
         self.debug = debug
+        self.pad_thresholds['self'] = t
 
     def epoch_sumup(self, *a, **kw):
         return '[{}]'.format(self.phase[:3]) + super().epoch_sumup(*a, **kw)
