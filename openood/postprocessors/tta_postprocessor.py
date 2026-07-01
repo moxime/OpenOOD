baeb760329877719a544ec6a1c9a6be18cc14156
moxime <mossonce@hotmail.com>
2026-07-01 17:46:05 +0200

    debug

--- openood/postprocessors/tta_postprocessor.py
+++ openood/postprocessors/tta_postprocessor.py
@@ -323,8 +323,7 @@ class TTAPostprocessor(BasePostprocessor):
         self.reset(net)
 
         print('***', len(data_loader))
-        print('***', data_loader.dataset)
-        batch = next(iter(data_loader))
+        print('***', data_loader.dataset[0])
         progress_bar = tqdm(data_loader,
                             dynamic_ncols=True,
                             disable=not progress or not comm.is_main_process())
