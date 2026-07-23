95bd3a7cde9f52d7aa428fe71c5159b522456955
moxime <mossonce@hotmail.com>
2026-07-23 11:21:21 +0200

    calculate conf

--- openood/postprocessors/ft_dist_tta_postprocessor.py
+++ openood/postprocessors/ft_dist_tta_postprocessor.py
@@ -99,10 +99,11 @@ class DistTTAPostprocessor(FTTTAPostprocessor):
 
     def calculate_conf(self, epoch=0, epochs=0):
 
-        if self.in_setup_thr_on_val and self.config.pipeline.partial < 0:
+        partial_ = self.config.pipeline.partial
+        if partial_ > 0. and partial_ <= 0.05:
             return epoch <= self.switch_phase or epoch == epochs
 
-        if self.partial > 0. and self.partial <= 0.2:
+        if self.in_setup_thr_on_val and partial_ < 0:
             return epoch <= self.switch_phase or epoch == epochs
 
         return epoch in (0, self.switch_phase, epochs)
